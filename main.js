c5a52b312abdf7d6549fdc2693effcc408429e85
CalebAurand <caurand13@gmail.com>
2022-03-29 22:27:39 -0500

    finished logic of project, final upload for submission

--- main.js
+++ main.js
@@ -13,7 +13,76 @@ const rl = readline.createInterface({
 
 const pigLatin = (word) => {
 
-  // Your code here
+  let trimWord = word.trim();
+  let lwWord = trimWord.toLowerCase();
+
+    let aIdx = lwWord.indexOf('a');
+    let eIdx = lwWord.indexOf('e');
+    let iIdx = lwWord.indexOf('i');
+    let oIdx = lwWord.indexOf('o');
+    let uIdx = lwWord.indexOf('u');
+    // let len = lwWord.length;
+
+    let lowestIndex = -1;
+
+    // console.log("The first 'a' is at position", aIdx);
+    // console.log("The first 'e' is at position", eIdx);
+    // console.log("The first 'i' is at position", iIdx);
+    // console.log("The first 'o' is at position", oIdx);
+    // console.log("The first 'u' is at position", uIdx);
+    // console.log("The length of the word is", len);
+
+    if(aIdx !== -1){
+        lowestIndex = aIdx;
+    } else if(eIdx !== -1){
+        lowestIndex = eIdx;
+    } else if(iIdx !== -1){
+        lowestIndex = iIdx;
+    } else if (oIdx !== -1){
+        lowestIndex = oIdx;
+    } else if (uIdx !== -1){
+        lowestIndex = uIdx;
+    };
+
+    if(eIdx !== -1 && eIdx<lowestIndex){
+        lowestIndex = eIdx;
+    }
+    if(iIdx !== -1 && iIdx<lowestIndex){
+        lowestIndex = iIdx;
+    }
+    if(oIdx !== -1 && oIdx<lowestIndex){
+        lowestIndex = oIdx;
+    }
+    if(uIdx !== -1 && uIdx<lowestIndex){
+        lowestIndex = uIdx;
+    }
+
+    console.log("The value of lowestIndex is: ", lowestIndex);
+
+    if(aIdx===-1 && eIdx===-1 && iIdx===-1 && oIdx===-1 && uIdx===-1){
+      console.log(lwWord + 'ay');
+      let newWord = lwWord + 'ay';
+      return newWord;
+
+    }else if(lowestIndex >= 1){
+        let array = lwWord.split('');
+        // console.log('array before for loop', array)
+        for(let i=0; i<lowestIndex; i++){
+            array[array.length+i] = array[i];
+        
+            // console.log(array);
+        }
+        for(let i=0; i<lowestIndex; i++){
+            array.shift();
+        }
+        let newWord = array.join('');
+        let newPigWord = newWord + 'ay';
+        console.log(newPigWord)
+        return newPigWord;
+    }else{
+        console.log(lwWord+'yay');
+        return lwWord+'yay';
+    };  
 
 }
 
